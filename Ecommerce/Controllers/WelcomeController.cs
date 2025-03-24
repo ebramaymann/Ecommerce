3b9cb809a303e23d8c38664e7b0b01ba0c64ace8
ebram <ebram.ayman73@gmail.com>
2025-03-25 00:32:44 +0200

    Add Welcome Controller

---
+++ Ecommerce/Controllers/WelcomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            //Data to be passed to the view
+            string Name = "Ebram";
+            int Age = 27;
+            double Salary = 5000;
+            
+            var person = new { Name, Age, Salary };
+
+            return View(model: person );
+            
+        }
+    }
+}
