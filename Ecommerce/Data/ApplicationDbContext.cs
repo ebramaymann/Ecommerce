317b1848ee85d7efd31b6accec19d46bf0c58e65
ebram <ebram.ayman73@gmail.com>
2025-03-27 15:33:44 +0200

    add ApplicationDbContext and models

---
+++ Ecommerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Brand> Brands { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-JUIUM6J\\SQLEXPRESS;Initial Catalog=Ecommerce512;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
