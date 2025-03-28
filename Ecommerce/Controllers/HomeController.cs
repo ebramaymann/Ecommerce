3f0da515ca1c21fdea69a7187cde684d13fd5a0b
ebram <ebram.ayman73@gmail.com>
2025-03-28 19:22:01 +0200

    configure preview data to retrieve from sql server data add Images folder

--- Ecommerce/Controllers/HomeController.cs
+++ Ecommerce/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 using System.Diagnostics;
+using Ecommerce.Data;
 using Ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _contex = new ();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -15,7 +18,9 @@ namespace Ecommerce.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var products = _contex.Products.Include(p => p.Brand).Include(p => p.Category);
+
+            return View(products.ToList());
         }
 
         public IActionResult Privacy()
